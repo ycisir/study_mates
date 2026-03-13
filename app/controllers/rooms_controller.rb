8e5f020ac22747158a4e4575fc1bcd67de167d7a
yasir <mohammadyasirkmt@gmail.com>
2026-03-13 09:00:19 +0530

    feat(room): implement room CRUD

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,48 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: %i[ show edit update destroy]
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room, notice: 'Room created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: 'Room updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to rooms_path, status: :see_other, notice: "Room deleted"
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.expect(room: [ :name, :description ])
+  end
+end
