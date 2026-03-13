713bb0b80ff2d4e9567d599691dd37914263f5f1
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2026-03-13 23:08:57 +0530

    feat(auth): install devise and generate user model (#8)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-  root "rooms#index"
+  devise_for :users
   resources :rooms
+  root "rooms#index"
 end
