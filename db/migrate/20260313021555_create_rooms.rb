ad5bd9b703e438cd747039bad102d262f91989b0
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2026-03-13 07:56:06 +0530

    Merge pull request #6 from ycisir/feat/room-model
    feat(room): create room model

---
+++ db/migrate/20260313021555_create_rooms.rb
@@ -0,0 +1,10 @@
+class CreateRooms < ActiveRecord::Migration[8.1]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_12_172232) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_13_021555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 end
