9e1d9a9eab5c0b713a43a0ccc5dcec3ed02e9dbb
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2026-03-16 15:06:33 +0530

    feat(auth): implement devise confirmable for account activation (#16)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_15_010951) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_014821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_15_010951) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "confirmation_sent_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,7 +35,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_15_010951) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.integer "role", default: 0
+    t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
