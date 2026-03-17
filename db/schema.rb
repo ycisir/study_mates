fc34a46bca932752ab21de6822cd11d6169c4e82
yasir <mohammadyasirkmt@gmail.com>
2026-03-17 14:55:06 +0530

    feat(topic): create topic model with name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_16_014821) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_17_092340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_16_014821) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
