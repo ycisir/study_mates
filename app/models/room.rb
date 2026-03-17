cd82f9b46697aee53ad73c2d5f82dbe9b404176f
yasir <mohammadyasirkmt@gmail.com>
2026-03-17 16:09:50 +0530

    feat(topic): create one to many association and data validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,19 @@
 class Room < ApplicationRecord
+	attr_accessor :topic_name
+
 	belongs_to :user
-	validates :name, presence: true
+	belongs_to :topic
+	validates :name, presence: { message: "is required" }
+	validates :topic_name, presence: { message: "is required" }
+	validates :description,
+            presence: { message: "is required" },
+            length: { minimum: 10, message: "must be at least 10 characters long" }
+	before_validation :clean_data
+
+	private
+
+	def clean_data
+	    self.name = name.to_s.titleize
+	    self.topic_name = topic_name.to_s.titleize
+	end
 end
