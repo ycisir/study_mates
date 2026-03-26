0d1575648d7ab8dddeb58582251f2c9104091d06
yasir <mohammadyasirkmt@gmail.com>
2026-03-26 15:35:20 +0530

    implement live search dropdown with clickable room links

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,19 +9,28 @@ class RoomsController < ApplicationController
 
     @rooms = Room.includes(:user, :topic)
 
-    # Search filter
-    if params[:q].present?
-      query = "%#{params[:q]}%"
-
-      @rooms = @rooms.joins(:topic).where('rooms.name ILIKE :q OR rooms.description ILIKE :q OR topics.name ILIKE :q', q: query)
-    end
-
     # Sidebar topic filter
     if params[:topic_id].present?
       @rooms = @rooms.where(topic_id: params[:topic_id])
     end
 
     @rooms = @rooms.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
+  end
+
+  def search
+    if params[:q].present?
+      query = "%#{params[:q]}%"
+
+      @rooms = Room.where('name ILIKE :q OR description ILIKE :q', q: query)
+    else
+      @rooms = []
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("search_results", partial: "rooms/search_results", locals: { rooms: @rooms })
+      end
+    end
   end
 
   def show
