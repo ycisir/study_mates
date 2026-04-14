0791d094752ddca4b02ee81bcdae2b18553497c0
yasir <mohammadyasirkmt@gmail.com>
2026-04-14 23:38:18 +0530

    Fix user feed query

--- app/models/concerns/feedable.rb
+++ app/models/concerns/feedable.rb
@@ -2,8 +2,24 @@ module Feedable
     extend ActiveSupport::Concern
 
     def feed
-        part_of_feed = "relationships.follower_id = :id or rooms.user_id = :id or rooms_users.user_id = :id"
-        Room.left_outer_joins(user: :followers).left_joins(:participants).where(part_of_feed, { id: id })
-        .distinct.includes(:user, :topic, messages: { files_attachments: :blob } )
+        part_of_feed = "
+            relationships.follower_id = :id
+            or rooms.user_id = :id
+            or rooms_users.user_id = :id
+            or messages.user_id = :id
+            or EXISTS (
+                  SELECT 1 FROM messages
+                  WHERE messages.room_id = rooms.id
+                  AND messages.user_id = :id
+                )
+            "
+        Room
+            .left_outer_joins(user: :followers)
+            .left_joins(:participants)
+            .left_joins(:messages)
+            .where(part_of_feed, { id: id })
+            .distinct
+            .includes(:user, :topic, messages: { files_attachments: :blob } )
+            .order(updated_at: :desc)
     end
 end
