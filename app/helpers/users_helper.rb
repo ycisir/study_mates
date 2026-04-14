be7d5d5c821b3f8dd2e00a387d81f66728f9fa4e
yasir <mohammadyasirkmt@gmail.com>
2026-04-14 23:19:57 +0530

    Add avatar_for helper and default avatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+	def avatar_for(user, variant: :thumb, size: "100x100")
+	  img = user.avatar.attached? ? user.avatar.variant(variant) : "default_avatar.png"
+	  image_tag url_for(img), class: "me-1", size: size
+	end
 end
