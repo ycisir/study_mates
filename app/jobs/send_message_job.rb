0efa8dd3b4a5317764a4304cfc415b23bcd482f4
= <=>
2026-08-25 15:48:24 +0530

    fix performing SendMessageJob error and counts

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -9,9 +9,11 @@ class SendMessageJob < ApplicationJob
       locals: { message: message }
     )
 
+    participants_list = room.participants.with_attached_avatar
+
     participants = ApplicationController.render(
       partial: "rooms/participants",
-      locals: { room: room }
+      locals: { room: room, participants: participants_list }
     )
 
     ActionCable.server.broadcast "room_chat_channel_#{message.room_id}", { html: html, participants: participants }
