f7f3926d2b49eda01b445b337c87ada09b87b3ce
Mohammad Yasir <mohammadyasirkmt@gmail.com>
2026-09-14 16:41:25 +0530

    fix docs/pdf download links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     api_key: ENV["SENDGRID_API_KEY"]
   }
 
-  config.active_storage.resolve_model_to = :proxy
+  # config.active_storage.resolve_model_to = :proxy
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
